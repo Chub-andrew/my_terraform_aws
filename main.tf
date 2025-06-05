fada40dad20d6fdc525cc077632b6b266a8e583c
Andrii Chubenko <stalkerock@gmail.com>
2025-06-05 12:16:18 +0300

    recreate for response 'Hello world'!!!

--- main.tf
+++ main.tf
@@ -1,14 +1,31 @@
 provider "aws" {
-    region = "eu-north-1"
+  region = "us-east-2"
 }
 
-
-# Amazon Machine Image(AMI) for the instance
 resource "aws_instance" "example" {
-    ami           = "ami-006b4a3ad5f56fbd6"
-    instance_type = "t3.micro"
+  ami = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-    tags = {
-        Name = "ExampleInstance"
-    }
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
